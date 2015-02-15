a9a5005c0d52d217c85dcd0b37ec2732dad5971a
Seth Lakowske <lakowske@gmail.com>
2015-02-15 05:31:53 -0600

    gulp and package file

--- index.js
+++ index.js
@@ -2,8 +2,33 @@
  * (C) 2015 Seth Lakowske
  */
 
-function handleDragStart(e) {
-    this.style.opacity = '0.4';
+var http = require('http');
+var JSONStream = require('JSONStream');
+
+var sample = document.querySelector('#req');
+
+function getRequests(host, port) {
+    var options = {
+        host : host,
+        port : port,
+        path : '/requests',
+        withCredentials : false
+    }
+
+    var parseify = JSONStream.parse();
+
+    var req = http.request(options, function(res) {
+        res.pipe(parseify);
+        parseify.on('data', function(dbrequest) {
+            sample.innerHtml = dbrequest.value;
+        })
+        res.on('data', function(dbrequest) {
+            sample.innerHtml = dbrequest.toString();
+        })
+
+
+    })
+    req.end();
 }
 
 var samples = document.querySelectorAll('.sample');
@@ -12,7 +37,6 @@ for (var i = 0 ; i < samples.length ; i++) {
     var el = samples[i];
     el.setAttribute('draggable', 'true');
     el.addEventListener('dragstart', function(e) {
-        console.dir(e);
         //e.srcElement.classList.add("dragging");
         e.dataTransfer.setData('text/plain', 'this may be dragged');
     });
@@ -22,7 +46,6 @@ var categories = document.querySelectorAll('.category');
 for (var i = 0 ; i < categories.length ; i++) {
     var el = categories[i];
     el.addEventListener('dragover', function(e) {
-        console.dir(e);
         if (e.toElement) e.toElement.classList.add("dropover");
         if (e.target) e.target.classList.add("dropover");
     })
@@ -31,5 +54,6 @@ for (var i = 0 ; i < categories.length ; i++) {
         if (e.toElement) e.toElement.classList.remove("dropover");
         if (e.target) e.target.classList.remove("dropover");
     })
-
 }
+
+getRequests('localhost', 3333);
--- server.js
+++ server.js
@@ -2,14 +2,31 @@
  * (C) 2015 Seth Lakowske
  */
 
-var  http = require('http');
-var ecstatic = require('ecstatic');
+var http       = require('http');
+var ecstatic   = require('ecstatic');
+var router     = require('routes')();
+var logger     = require('http-request-logger');
+var level      = require('level');
+var JSONStream = require('JSONStream');
+
+//open the request db
+var db            = level('./classified.db');
+var requestLogger = logger(db);
+var sink          = requestLogger.push();
 
 var st     = ecstatic({
     root : __dirname,
     baseDir : '/',
 })
 
+var parseify = JSONStream.parse();
+
+router.addRoute('/classify', function(req, res, params) {
+    req.pipe(parseify).pipe(sink);
+})
+
 http.createServer(function(req, res) {
-    st(req, res);
+    var m = router.match(req.url);
+    if (m) m.fn(req, res, m.params);
+    else st(req, res);
 }).listen(3000);
